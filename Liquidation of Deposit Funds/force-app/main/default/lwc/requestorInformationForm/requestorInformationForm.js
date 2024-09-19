9102fc508c6d0299c6ce0bf35e4e0d5198f5d715
Alexander Torres Rivera <Alexander.TorresRivera@popular.com>
2024-09-19 09:17:03 -0400

    09/19/2024 9:17AM

--- Liquidation of Deposit Funds/force-app/main/default/lwc/requestorInformationForm/requestorInformationForm.js
+++ Liquidation of Deposit Funds/force-app/main/default/lwc/requestorInformationForm/requestorInformationForm.js
@@ -4,19 +4,53 @@ import getCurrentUser from '@salesforce/apex/UserController.getCurrentUser';
 export default class RequestorInformationForm extends LightningElement {
 
     @api requestorInformationData;
+    relationshipSelectedValue = ''
+    relationshipOptions = [
+        { label: 'Relationship', value: '' },
+        { label: 'Father', value: 'Father' },
+        { label: 'Mother', value: 'Mother' },
+        { label: 'Brother', value: 'Brother' },
+        { label: 'Sister', value: 'Sister' },
+        { label: 'Son', value: 'Son' },
+        { label: 'Daughter', value: 'Daughter' },
+        { label: 'Grandfather', value: 'Grandfather' },
+        { label: 'Grandmother', value: 'Grandmother' },
+        { label: 'Grandson', value: 'Grandson' },
+        { label: 'Granddaughter', value: 'Granddaughter' },
+        { label: 'Uncle', value: 'Uncle' },
+        { label: 'Aunt', value: 'Aunt' },
+        { label: 'Nephew', value: 'Nephew' },
+        { label: 'Niece', value: 'Niece' },
+        { label: 'Cousin', value: 'Cousin' },
+        { label: 'Stepfather', value: 'Stepfather' },
+        { label: 'Stepmother', value: 'Stepmother' },
+        { label: 'Stepbrother', value: 'Stepbrother' },
+        { label: 'Stepsister', value: 'Stepsister' },
+        { label: 'Husband', value: 'Husband' },
+        { label: 'Wife', value: 'Wife' },
+        { label: 'Fiancé', value: 'Fiancé' },
+        { label: 'Fiancée', value: 'Fiancée' },
+        { label: 'Spouse', value: 'Spouse' },
+    ];
+    relationshipPHClass() {
+        const element = this.template.querySelector('.selector-input');
+        return element.value !== '' ? element.classList.remove('place-holder-text') : element.classList.add('place-holder-text');
+    }
 
-    async connectedCallback(){
-        try{
+    async connectedCallback() {
+        try {
             console.log("Getting User!")
             const record = await getCurrentUser();
-            this.requestorInformationData = {...this.requestorInformationData, 
-                email: record.Email?record.Email:'', 
-                preferredPhoneNumber: record.Phone?record.Phone:'', 
-                firstName: record.FirstName?record.FirstName:'', 
-                lastName: record.LastName?record.LastName:''}
+            this.requestorInformationData = {
+                ...this.requestorInformationData,
+                email: record.Email ? record.Email : '',
+                preferredPhoneNumber: record.Phone ? record.Phone : '',
+                firstName: record.FirstName ? record.FirstName : '',
+                lastName: record.LastName ? record.LastName : ''
+            }
             console.log("Success!!")
-        } catch(error){
-           console.error(error.message)
+        } catch (error) {
+            console.error(error.message)
         }
     }
 
@@ -28,12 +62,16 @@ export default class RequestorInformationForm extends LightningElement {
     preferredPhoneNumberErrorMessage = '';
     otherPhoneNumberErrorMessage = '';
 
-    handleChange(event){
+    handleChange(event) {
         const name = event.target.name;
         const value = event.target.value;
+        console.log('Event')
+        if(name === 'relationship'){
+            this.relationshipPHClass();
+        }
 
-        if(value == '' || value == null){
-            switch (name){
+        if (value == '' || value == null) {
+            switch (name) {
                 case 'firstName':
                     this.firstNameErrorMessage = 'Please enter a first name.';
                     break;
@@ -51,8 +89,8 @@ export default class RequestorInformationForm extends LightningElement {
                     break;
             }
             event.target.value = '';
-        }else{
-            
+        } else {
+
             this.firstNameErrorMessage = '';
             this.lastNameErrorMessage = '';
             this.relationshipErrorMessage = '';
